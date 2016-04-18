39ebc7fbc1022719b5764a336c071b3fff8aaf2d
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-04-18 15:07:46 -0700

    add users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex.js');
 
+function Users(){
+  return knex('users')
+}
+function Stories(){
+  return knex('stories')
+}
 /* GET users listing. */
-router.get('/:id', function(req, res, next) {
-  res.render('user');
+router.get('/:username', function(req, res, next) {
+  Users().first().where('username', req.params.username).then(function(user){
+    Stories().select().where('user_id', user.id).then(function(stories){
+      res.render('user', {user: user, userStories: stories})
+    })
+  })
+
 });
 
 
