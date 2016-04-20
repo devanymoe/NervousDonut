665bf58c1d0f1713c9c7dda91ea3be8d6d507e64
Jenni Bradstreet <jennic15@gmail.com>
2016-04-20 11:00:12 -0700

    Edit code, again.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req);
+  console.log(req.session.user);
   res.render('index', { title: 'Express', user: req.user });
 });
 
