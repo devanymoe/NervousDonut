a5a5b4177ae85568c44b167a1725c3d431c88f07
Mike Duin <michael.s.duin@gmail.com>
2016-04-20 11:58:36 -0700

    Change host variable

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL: process.env.HOST + "/auth/google/callback",
     scope: ['email', 'profile'],
     passReqToCallback: true
   },
