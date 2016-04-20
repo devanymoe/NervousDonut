fdc26a9ece12b978c6bf82956cad6b32991e24cc
Jenni Bradstreet <jennic15@gmail.com>
2016-04-20 10:45:03 -0700

    Edit code to get user data.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.user);
+  console.log(req.locals.user);
   res.render('index', { title: 'Express', user: req.user });
 });
 
